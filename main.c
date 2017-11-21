5ff4ae15f290e11af5b6d2b364fedd9d802c6f58
123 <1@www.sn.10086.cn>
2017-11-21 13:39:24 +0800

    add 3 dir

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 int main()
 {
+	int num=1;
+	int N;
+	int i=0;
+	printf("输入要求阶乘N值：");
+	scanf("%d",&N);
+	for(i=1;i<=N;i++)
+		num*=i;
 	printf("haha\n");
+	printf("N的阶乘为：%d\n",num);
 	return 0;
 }
 
